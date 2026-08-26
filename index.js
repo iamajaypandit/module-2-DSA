3dfc90ccd5b6b6887a953320e02fc41b284c5048
iamajaypandit <ap971092@gmail.com>
2026-08-26 11:55:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+// let n = 5;
+// let str="";
+// for(let i =1; i<=n; i++){
+//     str+="*";
+// }
+//  console.log(str);//***** 
+
+//  let n=4;
+//  for(let row=1; row<=n; row++){
+//     let str = "";
+//     for(let col=1; col<=5; col++){
+//         str += "*";
+//     }
+//     console.log(str);
+//  }
+//  //op=
+ //*****
+ //*****
+ //*****
+ //*****
+// let n =5;
+// let str ="";
+// for(let i =1; i<=n; i++){
+//     str+="*";
+//     console.log(str);
+// }
+
+// let n = 5;
+// for(let row=1; row<=n; row++){
+//  let str ="";
+//  for(let col=1; col<=row; col++){
+//     str+="*";
+//  }
+//  console.log(str);
+// } op=
+// *
+// **
+// ***
+// ****
+// let n = 4;
+// for(let row=1; row<=n; row++){
+//     let str ="";
+//     for(col=1; col<=2*row-1; col++){
+//         str+="*";
+//     }
+//     console.log(str);
+// }
+// odd series for -1 1 3 5... we use (2i-1) and for 1 3 5 7 we use (2i+1)
+
+// for even printing 
+// let n = 6;
+// for(let row=1; row<=n; row++){
+//     let str = "";
+//     for(let col =1; col<=2*row; col++){
+//         str+="*";
+//     }
+//     console.log(str);
+// }
+
+
+// let n = 5;
+// str ="";
+// for(let row=1; row<=n; row++){
+//  let str ="";
+//  for(let col=1; col<=n-row+1; col++){
+//     str+="*";
+//  }
+//  console.log(str);
+// }
+// *****
+// ****
+// ***
+// **
+// *
+let n=3;
+for(let row=n; row>=1; row--){
+ let str ="";
+ for(let col=1; col<=2*row-1; col++){
+    str+="*";
+ }
+ console.log(str);
+}
+
+// apprpoach 2
