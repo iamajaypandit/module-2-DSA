3085431d0906b3b1848d52d40c3df6db7a8d7f5d
iamajaypandit <ap971092@gmail.com>
2026-09-22 11:56:32 +0530

    calback anf=d high orser functions

--- day12/day12.js
+++ day12/day12.js
@@ -28,10 +28,13 @@ quickSort(arr, 0, arr.length - 1);
 console.log(arr);
 
 // time complexity
-// best case -> O(n log n)
+// best case -> O(n log2(n))
 // worst case -> O(n^2)
-// average case -> O(n log n)o(nlogn)
-// average case -> o(nlog)
+// average case -> O(nlog2(n))
+// space complexity -> 
+// best case ->O(nlog2(n))
+// average case ->O(nlog2(n))
+// worst case -> o(n)
 
 // let arr = [5, 4, 6, 7, 1, 2];
 
@@ -60,3 +63,90 @@ console.log(arr);
 
 // quickSort(arr, 0, arr.length - 1);
 // console.log(arr);
+
+
+
+// arrow function -> shorter way of writing functions in javascript
+// function add(a,b){
+//     console.log(a+b);
+// }
+// add(5,8);
+
+// const multiply = (a,b) =>a*b;
+// console.log(multiply(4,8));
+
+// const subtract = (c,d)=> c-d;
+// console.log(subtract(25,9));
+
+// Note -> functions in js is first class citizen. 
+// function can be stored variable can be passed in another as argument
+
+// callback functions -> function that is passed to another 
+// function as an argument is known as callback function.
+
+function delivery() {
+    console.log("item is sent for delivery");
+}
+
+function placeholder(status, callback) {
+    if (status === "successful") {
+        callback();
+    } else {
+        console.log("Order not successful");
+    }
+}
+
+placeholder("successful", delivery);
+
+
+// arrow function as a callback
+const multiply = (a, b) => {
+    return a * b;
+}
+
+const add = (a, b)=> {
+    return a + b;
+}
+
+const sub = (a,b)=> {
+    return a -b;
+}
+function calculate(a, b, operation) {
+    console.log(operation(a, b));
+}
+
+calculate(8, 9, multiply);
+calculate(5, 7, add);
+calculate(10, 5, sub);
+
+
+// functions can be returned frommanother function 
+// function buy(){
+//     return function payment(){
+//         console.log("payement is being processed");
+//     }
+// }
+// console.log(buy());
+
+// const process = buy();
+// process();
+// buy()();
+// [Function: payment]
+// payement is being processed
+// payement is being processed
+
+function buy(){
+    return function payment(){
+        return function delivery(){
+        console.log("item is being dlivered");
+    }
+}
+}
+console.log(buy());
+console.log(buy()());
+buy()()();
+// [Function: payment]
+// [Function: delivery]
+// item is being dlivered
+
+// Hingh Ordrer function -> 
